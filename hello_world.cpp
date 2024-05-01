6f27d30f4daab3dbd4c891a751ece4f4682af10d
xisseu <164639507+xisseu@users.noreply.github.com>
2024-05-01 18:10:04 +0300

    update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-cout << "Hello, World!" << endl;
+string name;
+getline(cin, name);
+cout << "Hello, World! from" << name << endl;
 return 0;
 }
