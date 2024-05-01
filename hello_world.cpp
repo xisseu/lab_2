d234c3196cfabfd820b643c73256bcfe5824c8dc
xisseu <164639507+xisseu@users.noreply.github.com>
2024-05-01 19:04:19 +0300

    hello_world.cpp without using namespace std;

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main()
 {
-string name;
-getline(cin, name);
-cout << "Hello, World! from" << name << endl;
+std::string name;
+getline(std::cin, name);
+std::cout << "Hello, World! from" << name << endl;
 return 0;
 }
